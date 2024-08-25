197cc278053e50f755c9698c9335cfa5247b001c
aminespinoza <amin.espinoza@outlook.com>
2024-08-24 19:29:29 -0600

    proyecto python actualizado

--- API_Python/app.py
+++ API_Python/app.py
@@ -15,7 +15,4 @@ def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
 
-@app.get("/consolas")
-def get_consolas():
-    rows = ["Xbox One", "Xbox 360", "Xbox Series S", "Xbos Series X" "Play Station 4", "Play Station 5", "Nintendo Switch", "Game Cube"]
-    return rows
+
